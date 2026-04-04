0a048d87c6d472d6a9434b37875228f7c1ee70fa
jett12-00 <yangkur59@gmail.com>
2026-04-04 20:06:12 +0700

    testing

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+
+
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << endl;
+        continue;
+    }
+
+    int i =5;
+    while (i<=5) {
+        cout << i << endl;
+        i++;
+        break;
+    }
+
+
+        int x;
+    do {
+
+        cout<<"input 1 - 2 - 3:"<<endl;
+        cin>>x;
+
+
+
+
+    }while(true);
+
+
+
+
+
+}
